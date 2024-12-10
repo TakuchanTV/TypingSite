5ac85a7875f110e804be0dd864c4138ea860c767
Taku <fk.53rk2@icloud.com>
2024-12-11 00:06:55 +0900

    タイマーの時間や正解数をカウントし、その正解数によって表示される文章を条件分岐で書いた

--- Typing/typing.ts
+++ Typing/typing.ts
@@ -2,29 +2,79 @@ const input= document.getElementById("input") as HTMLInputElement
 const Subject = document.getElementById("Subject") as HTMLInputElement
 const Right = document.getElementById("right") as HTMLInputElement
 const Wrong = document.getElementById("wrong") as HTMLInputElement
+const timer = document.getElementById("timer") as HTMLInputElement
+const Answer = document.getElementById("correctanswers") as HTMLElement
 
+let Time:number = 30;
+
+let count:number = 0;
+
+
+
+const countdown =  setInterval(() => {
+
+    timer.textContent = `制限時間: ${Time}秒`
+    Time--
+    if (Time < 0){
+        finish();
+    }
+},1000)
+
+const finish = () => {
+  
+
+   
+    clearInterval(countdown);
+    const fadeout = () => {
+            input.style.display = "none";
+            Subject.style.display = "none";
+            Right.style.display = "none";
+            Wrong.style.display = "none";
+    }
+
+    fadeout();
+    
+     setTimeout(() => {
+      if (count < 5) {
+          Answer.textContent = `正解数は${count}です。 もう少し頑張りましょう`
+          Answer.style.animation = "fadeup 2s ease-in-out forwards"
+      } else if (count < 10) {
+        Answer.textContent = `正解数は${count}です。 いいですね。その調子です`
+        Answer.style.animation = "fadeup 2s ease-in-out forwards"
+
+      }
+      else{
+        Answer.textContent = `正解数は${count}です。 素晴らしいです。よく頑張りましたね!`
+        Answer.style.animation = "fadeup 2s ease-in-out forwards"
+
+      }
+     },1000)
+    
+    
+    
+};
 
 
 
 const textList : string[] = [
-    "エステルとエーテルはなんか似ている",
+    "エステルとエーテル",
     "界面活性剤",
     "FT-IRでピークを見る",
-    "sp3混成軌道はメタンでしょ",
+    "sp3混成軌道",
     "バイオベースは難しい",
     "経験則はお友達",
     "メタノールは恐ろしい",
-    "分子は常に動いているよ",
-    "ヒドロキシ伸縮振動を探す",
-    "チャー収率はチャーシューは関係ない",
+    "ブラウン運動",
+    "ヒドロキシ伸縮振動",
+    "チャー収率",
     "TNTは爆弾よ",
     "UV照射光で修復させる",
     "キラルは語感が可愛い",
-    "ジスルフィド結合は欠かせない",
+    "ジスルフィド結合",
     "有機化学を勉強する",
     "ベンゼンをニトロ化する",
-    "触媒を入れて反応を促進させる",
-    "たんぱく質が熱で失活する",
+    "触媒を入れる",
+    "真空乾燥機",
     "高分子化学は難しい",
     "研究と勉強は違う",
     "白衣が汚れた",
@@ -35,7 +85,7 @@ const textList : string[] = [
     "研究職に憧れる",
     "フィルムを作製する",
     "ホスゲンは猛毒です",
-    "ハーバー・ボッシュ法は世界を救った",
+    "ハーバー・ボッシュ法",
     "一日撹拌する間に作業する",
     "反応の進行を確認した",
     "研究室に入り浸る",
@@ -51,12 +101,19 @@ const textList : string[] = [
     "教室から退出する",
     "教養を身に着ける",
     "遅刻しないようにしよう",
+    "機嫌を良くする",
+    "手袋を付ける",
+    "文献を探す",
+    "純水で洗う",
+    "学食で欧風カレー",
+    "工業大学生",
+    "お酒で酔いつぶれる",
+    "カラオケでオールする"
 
 ];
 
 
 const setRandomText = () => {
-    Subject.style.animation = "left-to-right 15s linear infinite"
     const rnd:number = Math.floor(Math.random()*textList.length)
     Subject.textContent = textList[rnd]
 }
@@ -78,17 +135,17 @@ Subject.addEventListener("animationstart", scheduleNextTextChange);
 const ShowInput = () => {
     console.log(input.value)
     if(input.value === Subject.textContent ){
-        let count:number = 0;
         Right.textContent = "正解です"
         count++;
-        console.log(count);
-        
+        console.log();
+        setRandomText();
         setTimeout(() => {
            Right.textContent = ""; 
-        },800)
+        },500)
     
     }else{
         Wrong.textContent = "不正解です" 
+        setRandomText();
         
         setTimeout(() => {
             Wrong.textContent = "";
